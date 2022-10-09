7caa75b732a1bec2c837fa283d2b42ef10957a35
clin1967 <clin1967@users.noreply.github.com>
2022-10-09 11:18:14 -0400

    Complete

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,6 +5,8 @@
  * 1. Introduction to Java helpful.
  */
 
+import java.util.ArrayList;
+
 public abstract class Bag {
     /*
      * TODO: Create the following private instance variables
@@ -13,7 +15,9 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
-
+    private String color;
+    int numberOfContents = 0, capacity;
+    ArrayList<String> contents = new ArrayList<>();
 
 
 
@@ -27,7 +31,10 @@ public abstract class Bag {
      * its contents.)
      */
 
-
+    public Bag(String color, int capacity){
+        this.capacity = capacity;
+        this.color = color;
+    }
 
 
     /*
@@ -37,8 +44,15 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
-
-
+    public String getColor(){
+        return this.color;
+    }
+    public int getNumberOfContents(){
+        return this.numberOfContents;
+    }
+    public int getCapacity(){
+        return this.capacity;
+    }
 
 
     /*
@@ -46,7 +60,9 @@ public abstract class Bag {
      *       color of this bag to the given color.
      */
 
-
+    public void setColor(String color){
+        this.color = color;
+    }
 
 
 
@@ -61,7 +77,14 @@ public abstract class Bag {
      *       and false otherwise.
      */
 
-
+    public boolean addItem(String item){
+        if (this.numberOfContents < this.capacity){
+            this.contents.add(item);
+            this.numberOfContents++;
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -75,7 +98,15 @@ public abstract class Bag {
      *
      * @return
      */
-
+    public String popItem(){
+        if (this.numberOfContents > 0) {
+            this.numberOfContents--;
+            String item = this.contents.get(this.numberOfContents);
+            this.contents.set(this.numberOfContents, null);
+            return item;
+        }
+        return null;
+    }
 
 
 
@@ -86,8 +117,7 @@ public abstract class Bag {
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+        this.capacity += n;
     }
 
     /**
--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -5,7 +5,27 @@
  * You may find the readings in chapter 3. Relationships between Classes
  * helpful while working through this exercise.
  */
+public class CrossbodyBag extends Bag{
+    public int numberOfStraps;
+    public CrossbodyBag(String color, int capacity, int numberOfStraps){
+        super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
+    }
 
+    public int getNumberOfStraps() {
+        return this.numberOfStraps;
+    }
+
+    public void enhance(){
+        super.increaseCapacity(2);
+    }
+
+    @Override
+    public String toString(){
+        return this.getColor() + " Crossbody Bag with " + this.numberOfStraps + " straps (" + this.numberOfContents + " / " +
+                this.capacity + ")";
+    }
+}
 /*
  * TODO: Create a public class named CrossbodyBag which is a subclass of Bag
  *       In addition to the attributes in Bag, the CrossbodyBag should have an
